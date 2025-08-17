1b865f85e4cdce983cf7de2bbd927eff3b6ea9f0
Michał Czerski <czerski.michal@gmail.com>
2025-08-17 09:11:16 +0200

    Update Errors.php

--- src/Form/Errors.php
+++ src/Form/Errors.php
@@ -2,9 +2,10 @@
 
 namespace Stormmore\Framework\Form;
 
+use ArrayAccess;
 use Stormmore\Framework\Validation\ValidationResult;
 
-class Errors
+class Errors implements ArrayAccess
 {
     private null|ValidationResult $validationResult;
 
@@ -22,4 +23,18 @@ class Errors
     {
         return $this->validationResult?->__get($name)?->message;
     }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return $this->validationResult?->__get($offset) != null;
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->validationResult?->__get($offset);
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void { }
+
+    public function offsetUnset(mixed $offset): void { }
 }
